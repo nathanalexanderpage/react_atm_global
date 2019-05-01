241e1dd5f88e3d97021b8d9c5fddcfa3069c80d8
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-05-01 07:02:12 -0700

    complete

--- react-atm/src/Account.js
+++ react-atm/src/Account.js
@@ -9,20 +9,45 @@ export default class Account extends Component {
     }
 
     this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawClick = this.handleWithdrawClick.bind(this)
   }
 
   handleDepositClick(e) {
     e.preventDefault();
     if (isNaN(this.refs.amount.value)) {
       console.log("Not a number");
-    }
-    else {
+    } else if (this.refs.amount.value < 0) {
+      console.log("Can't deposit negative amounts of money");
+    } else {
+      console.log(this.refs.amount.value);
       let amount = +this.refs.amount.value;
       let newBalance = this.state.balance + amount;
       this.setState({
         balance: newBalance
       })
       this.refs.amount.value = '';
+    }
+  }
+
+  handleWithdrawClick(e) {
+    e.preventDefault();
+    console.log(this.refs.amount.value);
+
+    if (isNaN(this.refs.amount.value)) {
+      console.log("Not a number");
+    } else if (this.refs.amount.value < 0) {
+      console.log("Can't withdraw negative amounts of money");
+    } else {
+      let amount = +this.refs.amount.value;
+      let newBalance = this.state.balance - amount;
+      if (newBalance < 0) {
+        console.log("insufficient funds, bucko");
+      } else {
+        this.setState({
+          balance: newBalance
+        })
+        this.refs.amount.value = '';
+      }
     }
   }
 
@@ -38,6 +63,7 @@ export default class Account extends Component {
         <div className={balanceClass}>${this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref="amount" />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
       </div>
     )
   }
